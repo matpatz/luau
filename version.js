c8935e810bbf05f52d9cea487ff22d89451bb4fd
VoltexHub <webingwme@gmail.com>
2025-08-31 22:32:51 -0500

    Update version.js

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 return {
-    ['126884695634066'] = { version=2, updated='2025-08-31' },
-    ['53453465134142342'] = { version=8, updated='2025-08-31' }
+    ['117289497495248'] = { version=2, updated='2025-08-31' }, -- trident
+    ['53453465134142342'] = { version=8, updated='2025-08-31' } -- grow a garden? anyways its not real
 }
