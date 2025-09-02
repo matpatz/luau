6d8daed05c4780493a2c2ae20a9b2aa722112da3
VoltexHub <webingwme@gmail.com>
2025-09-02 18:42:43 -0500

    Delete example.lua

--- example.lua
+++
@@ -1,9 +0,0 @@
-local api = loadstring(game:HttpGet("https://website-api-bay.vercel.app/version.js"))()
-local data = api[tostring(game.PlaceId)]
-
-if data then
-    print("Version:", data.version)
-    print("Game last update:", data.updated)
-else
-    warn("Invalid game.. you need to join one of our supported games")
-end
