65cf23d9977287e8f747964be02d722e357f57d5
VoltexHub <webingwme@gmail.com>
2025-09-01 22:33:27 -0500

    Update version.js

--- version.js
+++ version.js
@@ -3,6 +3,12 @@
     "version": 1,
     "updated": "2025-09-01",
     "status": "Functional",
+    "supported": {
+      "Potassium",
+      "Delta",
+	  "Xeno"
+	  "Solara"
+    },
     "devs": {
       "matpats": "B86F5628-12B8-4683-93C7-773F70D1A406",
       "john": "thisisafakehwid"
@@ -12,6 +18,12 @@
     "version": 2,
     "updated": "2025-09-01",
     "status": "Functional",
+	    "supported": {
+      "Potassium",
+      "Delta",
+	  "Xeno"
+	  "Solara"
+    },
     "devs": {
       "matpats": "B86F5628-12B8-4683-93C7-773F70D1A406",
       "tod": "realhwid1234"
