8abbda9a2e50c086a5e5f2164e89e3c00c6ffc1e
VoltexHub <webingwme@gmail.com>
2025-09-01 22:16:08 -0500

    Update version.js

--- version.js
+++ version.js
@@ -1,24 +1,20 @@
-const api = {
-    "168556275": { //-- Baseplate --\\
-        version: 1,
-        updated: "2025-09-01",
-        status: "Functional",
-        devs: {
-            matpats: "B86F5628-12B8-4683-93C7-773F70D1A406",
-            john: "thisisafakehwid"
-        }
-    },
-    "53453535": { //-- placeholder --\\
-        version: 2,
-        updated: "2025-09-01",
-        status: "Functional",
-        devs: {
-            matpats: "B86F5628-12B8-4683-93C7-773F70D1A406",
-            tod: "realhwid1234"
-        }
+{
+  "168556275": {
+    "version": 1,
+    "updated": "2025-09-01",
+    "status": "Functional",
+    "devs": {
+      "matpats": "B86F5628-12B8-4683-93C7-773F70D1A406",
+      "john": "thisisafakehwid"
     }
-};
-
-if (typeof module !== "undefined" && module.exports) {
-    module.exports = api;
+  },
+  "53453535": {
+    "version": 2,
+    "updated": "2025-09-01",
+    "status": "Functional",
+    "devs": {
+      "matpats": "B86F5628-12B8-4683-93C7-773F70D1A406",
+      "tod": "realhwid1234"
+    }
+  }
 }
