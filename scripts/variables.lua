d5f674c75609873336c5df56f0b2e4550d16e2c2
VoltexHub <webingwme@gmail.com>
2025-09-04 17:00:01 -0500

    Delete scripts directory

--- scripts/variables.lua
+++
@@ -1,9 +0,0 @@
-do
-    for _, service in ipairs(game:GetChildren()) do
-        if pcall(function() return service:IsA("Service") end) then
-            localName = service.Name:gsub("%s", "")
-            _G[localName] = service
-            _G[localName] = service 
-        end
-    end
-end
