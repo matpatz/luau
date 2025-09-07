19a4350b4dc3ad588793d00fe96cd410c0d09fd3
VoltexHub <webingwme@gmail.com>
2025-09-07 00:07:58 -0500

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,22 +1,28 @@
-{
+// /api/data.js
+const data = {
   "168556275": {
-    "version": 1,
-    "updated": "2025-09-01",
-    "status": "Functional",
-    "supported": ["Potassium","Delta","Xeno","Solara"],
-    "devs": {
-      "matpats": "B86F5628-12B8-4683-93C7-773F70D1A406",
-      "john": "thisisafakehwid"
+    version: 1,
+    updated: "2025-09-01",
+    status: "Functional",
+    supported: ["Potassium","Delta","Xeno","Solara"],
+    devs: {
+      matpats: "B86F5628-12B8-4683-93C7-773F70D1A406",
+      john: "thisisafakehwid"
     }
   },
   "53453535": {
-    "version": 2,
-    "updated": "2025-09-01",
-    "status": "Functional",
-    "supported": ["Potassium","Delta","Xeno","Solara"],
-    "devs": {
-      "matpats": "B86F5628-12B8-4683-93C7-773F70D1A406",
-      "tod": "realhwid1234"
+    version: 2,
+    updated: "2025-09-01",
+    status: "Functional",
+    supported: ["Potassium","Delta","Xeno","Solara"],
+    devs: {
+      matpats: "B86F5628-12B8-4683-93C7-773F70D1A406",
+      tod: "realhwid1234"
     }
   }
+};
+
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.status(200).json(data);
 }
