f5043c94db227cc108305601d82fc694d362bc19
VoltexHub <webingwme@gmail.com>
2025-09-07 23:10:58 -0500

    Update jackpot.js

--- public/code/scripts/powerball/jackpot.js
+++ public/code/scripts/powerball/jackpot.js
@@ -1,5 +1,21 @@
-{
-  "whites": [4, 15, 23, 42, 51],
-  "powerball": 12,
-  "jackpot": 50000000
+import { promises as fs } from "fs";
+
+export default async function handler(req, res) {
+  const filePath = './jackpot.json';
+  let data = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+
+  if (req.method === 'POST') {
+    const { whites, powerball, jackpot } = req.body;
+    if (whites && powerball && jackpot != null) {
+      data.whites = whites;
+      data.powerball = powerball;
+      data.jackpot = jackpot;
+      await fs.writeFile(filePath, JSON.stringify(data, null, 2));
+      return res.status(200).json({ success: true });
+    }
+    return res.status(400).json({ error: 'Invalid body' });
+  }
+
+  // GET request returns current jackpot + winning numbers
+  res.status(200).json(data);
 }
