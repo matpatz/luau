fb36bb5466001df3c111c34e87800db2cc00af17
VoltexHub <webingwme@gmail.com>
2025-09-13 12:05:14 -0500

    Update message.js

--- api/message.js
+++ api/message.js
@@ -1,1 +1,8 @@
-[]
+export default function handler(req, res) {
+  const messages = [
+    { Player: "Alice", Message: "Hello everyone!" },
+    { Player: "Bob", Message: "Hey Alice!" }
+  ];
+
+  res.status(200).json(messages);
+}
