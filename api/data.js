bf097a6f2d429c2b82aa22be4669bab1d91626cf
VoltexHub <webingwme@gmail.com>
2025-09-13 23:45:52 -0500

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,27 +1,22 @@
-const data = {
-  "108470888": {
-    version: 1,
-    updated: "2025-09-01",
-    status: "Functional",
-    supported: ["Potassium","Delta","Xeno","Solara"],
-    devs: {
-      matpats: "B86F5628-12B8-4683-93C7-773F70D1A406",
-      john: "thisisafakehwid"
+{
+  "Catastrophia": { 
+    "version": 1,
+    "updated": "2025-09-01",
+    "status": "Functional",
+    "supported": ["Potassium","Delta","Xeno","Solara"],
+    "devs": {
+      "matpats": "B86F5628-12B8-4683-93C7-773F70D1A406",
+      "john": "thisisafakehwid"
     }
   },
-  "53453535": {
-    version: 2,
-    updated: "2025-09-01",
-    status: "Functional",
-    supported: ["Potassium","Delta","Xeno","Solara"],
-    devs: {
-      matpats: "B86F5628-12B8-4683-93C7-773F70D1A406",
-      tod: "realhwid1234"
+  "game": { 
+    "version": 2,
+    "updated": "2025-09-01",
+    "status": "Functional",
+    "supported": ["Potassium","Delta","Xeno","Solara"],
+    "devs": {
+      "matpats": "B86F5628-12B8-4683-93C7-773F70D1A406",
+      "tod": "realhwid1234"
     }
   }
-};
-
-export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json(data);
 }
