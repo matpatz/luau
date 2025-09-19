ac7c6e22474d34a80b6080a7ee29831844ab6665
VoltexHub <webingwme@gmail.com>
2025-09-19 17:06:11 -0500

    Update message.js

--- api/message.js
+++ api/message.js
@@ -1,1 +1,15 @@
-efwf
+let messages = []
+
+export default function handler(req, res) {
+  const { player, message } = req.query
+
+  if (player && message) {
+    messages.push({
+      Date: new Date().toISOString(),
+      Player: player,
+      Message: message
+    })
+  }
+
+  res.status(200).json(messages)
+}
