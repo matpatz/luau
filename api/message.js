47b29024adf0431efc43f3fc7885926b984c1b7d
VoltexHub <webingwme@gmail.com>
2025-09-19 17:05:13 -0500

    Update message.js

--- api/message.js
+++ api/message.js
@@ -1,11 +1,14 @@
-// api/message.js
 let messages = []
 
 export default function handler(req, res) {
   const { player, message } = req.query
 
   if (player && message) {
-    messages.push({ Player: player, Message: message })
+    messages.push({
+      Date: new Date().toISOString(),
+      Player: player,
+      Message: message
+    })
   }
 
   res.status(200).json(messages)
