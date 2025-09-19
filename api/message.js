c64dbbd8449cd8d19990ed5c03a9aae335b44051
VoltexHub <webingwme@gmail.com>
2025-09-19 16:59:42 -0500

    Update message.js

--- api/message.js
+++ api/message.js
@@ -1,13 +1,12 @@
 // api/message.js
-import { kv } from '@vercel/kv'
+let messages = []
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const { player, message } = req.query
 
   if (player && message) {
-    await kv.rpush('chat:messages', JSON.stringify({ Player: player, Message: message }))
+    messages.push({ Player: player, Message: message })
   }
 
-  const all = await kv.lrange('chat:messages', -50, -1) // last 50 messages
-  res.status(200).json(all.map(JSON.parse))
+  res.status(200).json(messages)
 }
