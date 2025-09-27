d8fffa93a64637b8074cb9e3a7bb6ddf95e7391f
VoltexHub <webingwme@gmail.com>
2025-09-27 18:34:52 -0500

    Create glist.js

---
+++ public/code/wh/glist.js
@@ -0,0 +1,5 @@
+const wh = {
+    "GameName1": ["https://discord.com/api/webhooks/webhookurl123"],
+    "GameName2": ["https://discord.com/api/webhooks/webhookurl456"],
+    "GameName3": ["https://discord.com/api/webhooks/webhookurl789"]
+};
