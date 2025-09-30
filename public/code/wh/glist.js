2d4c47fa326b599f716b9be37b3208bf005eae38
VoltexHub <webingwme@gmail.com>
2025-09-30 17:44:12 -0500

    Delete public/code/wh directory

--- public/code/wh/glist.js
+++
@@ -1,5 +0,0 @@
-const wh = {
-    "GameName1": ["https://discord.com/api/webhooks/webhookurl123"],
-    "GameName2": ["https://discord.com/api/webhooks/webhookurl456"],
-    "GameName3": ["https://discord.com/api/webhooks/webhookurl789"]
-};
