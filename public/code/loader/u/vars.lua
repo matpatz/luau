4044d4ab8e31e4815551ac8153015ca3e158b6d9
VoltexHub <webingwme@gmail.com>
2025-11-15 00:32:40 -0600

    Update vars.lua

--- public/code/loader/u/vars.lua
+++ public/code/loader/u/vars.lua
@@ -1,1 +1,6 @@
-getgenv().v or {}
+local v, s, get = {}, {players = "Players", workspace = "Workspace", rs = "RunService", core = "CoreGui", input = "UserInputService", rep = "ReplicatedStorage", lighting = "Lighting", vim = "VirtualInputManager", vcr = "TextChatService"},  (type(cloneref) == "function") and cloneref or function(x) return x end
+for short, name in pairs(s) do
+    v[short] = get(game:GetService(name))
+end
+
+return v
