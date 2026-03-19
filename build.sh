1fde5a2f664526f0e5d270569b07106d0313534b
VoltexHub <webingwme@gmail.com>
2026-03-18 19:32:50 -0500

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -fsSL https://github.com/lune-org/lune/releases/download/v0.8.9/lune-0.8.9-linux-x86_64.zip -o lune.zip
-unzip lune.zip
-chmod +x lune
-mkdir -p bin
-mv lune bin/
-export PATH="$PWD/bin:$PATH"
