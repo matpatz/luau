7d1c1f78a574e2e61bad154e19799456ec6d9007
matpat <webingwme@gmail.com>
2026-05-05 10:48:13 -0500

    Delete public/code/scripts/obfuscator directory

--- public/code/scripts/obfuscator/example.lua
+++
@@ -1,2 +0,0 @@
-local reconstruct = loadstring(game:HttpGet("https://website-iota-ivory-12.vercel.app/code/scripts/obfuscator/main.lua"))()
-reconstruct("loadstring(game:HttpGet(`https://pastefy.app/XCumBhfc/raw`))()")
