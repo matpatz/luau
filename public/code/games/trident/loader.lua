641dfd9410ea005ed83820c4bd0f9103a744995e
matpat <webingwme@gmail.com>
2026-06-13 00:31:34 -0500

    Update loader.lua

--- public/code/games/trident/loader.lua
+++ public/code/games/trident/loader.lua
@@ -1,2 +1,2 @@
-local source = game:HttpGet("https://website-iota-ivory-12.vercel.app/code/games/trident/main.luau")
+local source = game:HttpGet("https://website-iota-ivory-12.vercel.app/code/games/trident/main.lua")
 run_on_actor(getactors()[1], source)
